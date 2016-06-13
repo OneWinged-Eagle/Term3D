afcc0bcea4fa69eeab18cc33be49521339554cb7
William Nayrole <nayrol_w@epitech.eu>
2016-06-13 12:49:39 +0100

    StreamCallbacks

--- Assets/Scripts/Network/ServerCallback.cs
+++ Assets/Scripts/Network/ServerCallback.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using UdpKit;
+using Bolt;
 
 [BoltGlobalBehaviour(BoltNetworkModes.Host)] //change to .Server if there is a bug
 public class ServerCallback : Bolt.GlobalEventListener
@@ -9,6 +12,7 @@ public class ServerCallback : Bolt.GlobalEventListener
         var log = EventLog.Create();
         log.Message = string.Format("{0} connected", connection.RemoteEndPoint);
         log.Send();
+        connection.SetStreamBandwidth(1024 * 20);
     }
 
     public override void Disconnected(BoltConnection connection)
@@ -19,3 +23,26 @@ public class ServerCallback : Bolt.GlobalEventListener
     }
 }
 
+[BoltGlobalBehaviour()]
+public class StreamCallbacks : Bolt.GlobalEventListener
+{
+    public static UdpKit.UdpChannelName testChannel;
+    public override void BoltStartBegin()
+    {
+        testChannel = BoltNetwork.CreateStreamChannel("test", UdpKit.UdpChannelMode.Reliable, 1);
+    }
+
+    public override void SceneLoadRemoteDone(BoltConnection c)
+    {
+        if (BoltNetwork.isServer)
+        {
+            byte[] data = Encoding.ASCII.GetBytes("Testing");
+            c.StreamBytes(testChannel, data);
+        }
+    }
+
+    public override void StreamDataReceived(BoltConnection connection, UdpStreamData data)
+    {
+        BoltLog.Info(data);
+    }
+}
