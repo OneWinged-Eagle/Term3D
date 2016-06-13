a3299cc6e31725c9f3a959384b4e21004499aa7e
Victor <victorpichon@free.fr>
2016-06-14 01:07:39 +0200

    des petits bails de cam et d'instantiante

---
+++ Assets/Scripts/Camera/moveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCamera : MonoBehaviour {
+	public GameObject player;
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rotateSpeed = 1f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(Input.GetAxis("Mouse X"));
+		//transform.Rotate (Input.GetAxis ("Mouse Y"), Input.GetAxis("Mouse X")  * Time.deltaTime * rotateSpeed, 0f); 
+		transform.RotateAround(player.transform.position, new Vector3(1.0f, 0.0f, 0.0f), Input.GetAxis("Mouse Y") * 100 *Time.deltaTime);
+		//transform.RotateAround (player.transform.position, new Vector3 (0.0f, 1.0f, 0.0f), Input.GetAxis ("Mouse X") * 100 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Term3D/Personnage/addElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addElement : MonoBehaviour {
+	public Transform spawnPoint;
+	public GameObject spawnObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			Debug.Log ("obj spawn");
+			Instantiate (spawnObject, spawnPoint.position, spawnPoint.rotation);
+		}
+
+			
+	
+	}
+}
