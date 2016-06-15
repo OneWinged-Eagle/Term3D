ed0a3c491e20460c020f452c7b74157e098ff814
Victor <victorpichon@free.fr>
2016-06-15 23:24:18 +0200

    cam a la premiere personne ajout et  deplacement dobjet

--- Assets/Scripts/Term3D/Personnage/addElement.cs
+++ Assets/Scripts/Term3D/Personnage/addElement.cs
@@ -5,20 +5,54 @@ public class addElement : MonoBehaviour {
 	public Transform spawnPoint;
 	public GameObject spawnObject;
 
+	public float lenghtRay;
+
+	public GameObject cameraObj;
+
+	public float x;
+	public float y;
+	public float z;
+
+
+	private Quaternion rot;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.E))
-		{
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.E)) {
 			Debug.Log ("obj spawn");
 			Instantiate (spawnObject, spawnPoint.position, spawnPoint.rotation);
 		}
 
-			
-	
+		RaycastHit hit;
+		Ray intersectionRay = Camera.main.ScreenPointToRay (new Vector3 (Screen.width * 0.5f, Screen.height * 0.5f, 0.0f));
+
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("ça appuie");
+			if (Physics.Raycast (intersectionRay, out hit, lenghtRay)) {
+				if (hit.collider.tag == "Environment")
+					Debug.Log ("ça otuche" + hit.collider.tag);
+				if (hit.collider.tag == "NonStaticObject") {
+					Debug.Log ("ça otuche" + hit.collider.tag);
+					hit.transform.SendMessage ("pickUp", true, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+
+
+		//pas propre ici a refaire
+		else if (Input.GetMouseButtonDown (1)) {
+			if (Physics.Raycast (intersectionRay, out hit, lenghtRay)) {
+				if (hit.collider.tag == "NonStaticObject") {
+					Debug.Log ("ça otuche" + hit.collider.tag);
+					hit.transform.SendMessage ("pickUp", false, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
 	}
 }
---
+++ Assets/pickUpObj.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpObj : MonoBehaviour {
+
+	public GameObject hook;
+
+	private bool picked;
+
+	// Use this for initialization
+	void Start () {
+		
+		hook = GameObject.Find ("Hook");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (picked == true) {
+			transform.position = hook.transform.position;
+			transform.rotation = hook.transform.rotation;
+		}
+	
+	}
+
+	public void pickUp(bool pickedUp)
+	{
+		Debug.Log ("coucou ça otuche ici hihi");
+
+		picked = pickedUp;
+	}
+}
