8b7b502571d485fbd4d2c24f2e4d3f5a3f2a5f1f
Guillaume Szwankowski <guillaume.szwankowski@epitech.eu>
2016-07-04 19:59:29 +0200

    Merged?

--- Assets/Scripts/Term3D/InitWolrd.cs
+++ Assets/Scripts/Term3D/InitWolrd.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.IO;
 
 [BoltGlobalBehaviour]
 public class testCallbacks : Bolt.GlobalEventListener
@@ -16,8 +18,7 @@ public class testCallbacks : Bolt.GlobalEventListener
 	public override void SceneLoadLocalDone(string map)
 	{
 		// randomize a position
-		Vector3 pos = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
-
+		Vector3 pos = new Vector3(UnityEngine.Random.Range(-16, 16), 0, UnityEngine.Random.Range(-16, 16));
 
 		BoltNetwork.Instantiate(BoltPrefabs.Robot, pos, Quaternion.identity);
 		//BoltNetwork.Instantiate(BoltPrefabs.Player, pos, Quaternion.identity);
@@ -26,19 +27,44 @@ public class testCallbacks : Bolt.GlobalEventListener
 			instantiateWorld ();
 
 	}
-		
+
+	private bool init(string root)
+	{
+		if (!Directory.Exists(root))
+			return false;
+
+		PathUtils.setRootPath(root);
+		PathUtils.setCurrPath(root);
+		DirectoryUtils.Directory rootDir = new DirectoryUtils.Directory(root);
+
+		FileUtils.File[] files = rootDir.getFiles();
+		for (uint i = 0; i < files.Length; ++i)
+			//Debug.Log(string.Format("File name is {0} with extension {1}\nFile content: {2}", file.getFileNameWithoutExtension(), file.getExtension(), file.getContent()));
+			BoltNetwork.Instantiate(BoltPrefabs.Document, new Vector3 (20f, 0.5f, 5.0f * (2 + i)), Quaternion.identity);
+
+		DirectoryUtils.Directory[] directories = rootDir.getDirectories();
+		for (uint i = 0; i < directories.Length; ++i)
+			BoltNetwork.Instantiate(BoltPrefabs.Cube_rouge, new Vector3 (25f, 0.5f, 25f + (2 * i)), Quaternion.identity);
+
+	 return true;
+	}
+
 	public void instantiateWorld()
 	{
 		BoltNetwork.Instantiate(BoltPrefabs.Terrain, new Vector3 (0, 0, 0), Quaternion.identity);
-		BoltNetwork.Instantiate(BoltPrefabs.Cube_rouge, new Vector3 (20f, 0.5f, 20f), Quaternion.identity);
+		/*BoltNetwork.Instantiate(BoltPrefabs.Cube_rouge, new Vector3 (20f, 0.5f, 20f), Quaternion.identity);
 		BoltNetwork.Instantiate(BoltPrefabs.Cube_vert, new Vector3 (20f, 0.5f, 15f), Quaternion.identity);
 		BoltNetwork.Instantiate(BoltPrefabs.Sphere, new Vector3 (25f, 0.5f, 25f), Quaternion.identity);
-		BoltNetwork.Instantiate(BoltPrefabs.Cylindre, new Vector3 (20f, 0.5f, 30f), Quaternion.identity);
+		BoltNetwork.Instantiate(BoltPrefabs.Cylindre, new Vector3 (20f, 0.5f, 30f), Quaternion.identity);*/
 
 
+		if (init("C:\\development\\test"))
+			Debug.log("Everything OK!");
+		else
+			Debug.log("init(\"C:\\development\\test\") failed...");
 
 
-		BoltEntity test= BoltNetwork.Instantiate (BoltPrefabs.Cube_rouge, new Vector3 (10f, 0.5f, 10f), Quaternion.identity);
+		//BoltEntity test= BoltNetwork.Instantiate (BoltPrefabs.Cube_rouge, new Vector3 (10f, 0.5f, 10f), Quaternion.identity);
 
 
 
@@ -53,7 +79,7 @@ public class testCallbacks : Bolt.GlobalEventListener
 
 /*
 
-	
+
 
 
 		objs [0] = GameObject.Find("Cube vert");
---
+++ Assets/Scripts/Term3D/Utils/DirectoryUtils.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class DirectoryUtils
+{
+	public class Directory : PathUtils.Path
+	{
+		public Directory(string path) : base(path) {}
+
+		public string getDirName()
+		{
+			return this._path;
+		}
+
+		public FileUtils.File[] getFiles()
+		{
+			List<FileUtils.File> filesList = new List<FileUtils.File>();
+			String[] files = System.IO.Directory.GetFiles(this._path);
+			foreach (string file in files)
+				filesList.Add(new FileUtils.File(file));
+			return filesList.ToArray();
+		}
+
+		public Directory[] getDirectories()
+		{
+			List<Directory> directoriesList = new List<Directory>();
+			String[] directories = System.IO.Directory.GetDirectories(this._path);
+			foreach (string directory in directories)
+				directoriesList.Add(new Directory(directory));
+			return directoriesList.ToArray();
+		}
+	}
+}
