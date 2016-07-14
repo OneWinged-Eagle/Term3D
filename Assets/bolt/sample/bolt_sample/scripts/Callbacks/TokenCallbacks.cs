19c7e745f4c437c218985a8d87ff3c134d2994cb
Victor <victorpichon@free.fr>
2016-07-14 20:10:23 +0200

    on repar praupre lol

--- Assets/bolt/sample/bolt_sample/scripts/Callbacks/TokenCallbacks.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[BoltGlobalBehaviour]
-public class TokenCallbacks : Bolt.GlobalEventListener {
-  public override void BoltStartBegin() {
-    BoltNetwork.RegisterTokenClass<TestToken>();
-  }
-}
