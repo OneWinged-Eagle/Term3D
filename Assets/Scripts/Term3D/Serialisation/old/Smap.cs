9ea062f1c9fbf51e6456f803ac17784e48185300
Victor <victorpichon@free.fr>
2016-07-14 17:57:01 +0200

    old seriliasation

---
+++ Assets/Scripts/Term3D/Serialisation/old/Smap.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+
+using System;
+using System.IO;
+
+// l'ultime qui class qui contient toutes les rooms 
+public class Smap {
+
+	//public int coord { get; set;}
+	public string name;
+	public List<XRoom> rooms = new List<XRoom>();
+}
