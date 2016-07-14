c1c4c34eba0eb74be13f82ebe0c6f996e12c8c96
William Nayrole <nayrol_w@epitech.eu>
2016-07-14 18:31:37 +0100

    commit streamcallback

--- Assets/Scripts/Network/StreamCallback.cs
+++ Assets/Scripts/Network/StreamCallback.cs
@@ -8,37 +8,25 @@ using Bolt;
 [BoltGlobalBehaviour()]
 public class StreamCallbacks : Bolt.GlobalEventListener
 {
-    public static UdpKit.UdpChannelName testChannel;
+    public static UdpKit.UdpChannelName audioChannel;
     public override void BoltStartBegin()
     {
-        testChannel = BoltNetwork.CreateStreamChannel("test", UdpKit.UdpChannelMode.Reliable, 1);
+        audioChannel = BoltNetwork.CreateStreamChannel("audio", UdpKit.UdpChannelMode.Reliable, 1);
     }
 
     public override void SceneLoadRemoteDone(BoltConnection c)
     {
         if (BoltNetwork.isServer)
         {
-            /*
-            string text;
-            var fileStream = new FileStream(@"c:\testin.mp3", FileMode.Open, FileAccess.Read);
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
-            {
-                text = streamReader.ReadToEnd();
-            }
-            */
             byte[] data = File.ReadAllBytes(@"c:\testin.mp3");
-            BoltLog.Info("DATA TEST" + data.ToString() + " " + c);
-            c.StreamBytes(testChannel, data);
+            BoltLog.Info("DATA TEST");
+            c.StreamBytes(audioChannel, data);
         }
     }
 
     public override void StreamDataReceived(BoltConnection connection, UdpStreamData data)
     {
-        BoltLog.Info("TEST CLIENT");
-        BoltLog.Info("DATA SENT: "/*+ System.Text.Encoding.UTF8.GetString(data.Data)*/);
+        BoltLog.Info("CLIENT GOT DATA ! :)");
         System.IO.File.WriteAllBytes(@"c:\testout.mp3", data.Data);
-        //var sound = new GameObject();
-        //AudioCube aud = sound.GetComponent<AudioCube>();
-        //aud.setClipByUrl(url);
     } 
 }
