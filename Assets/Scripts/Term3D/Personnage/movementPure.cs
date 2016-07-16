393c1cf7aa07bf91a0ae88115b883a7f90e84b70
kihel_w <wadi.kihel@epitech.eu>
2016-07-16 21:07:14 +0200

    add Player and Camera pure

---
+++ Assets/Scripts/Term3D/Personnage/movementPure.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementPure : MonoBehaviour {
+
+	public float moveSpeed;
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+		//rotateSpeed = 1.0f;
+
+		print ("player init");
+	}
+
+	// Update is called once per frame
+		void Update () {
+		transform.Translate (moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+		transform.Rotate (Input.GetAxis ("Mouse Y") * Time.deltaTime * rotateSpeed, Input.GetAxis("Mouse X"), 0f); 
+	}
+}
