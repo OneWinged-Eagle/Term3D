0ef31f98a24e69c6a070f5211c19eded4a0b720c
William Nayrole <nayrol_w@epitech.eu>
2016-09-16 17:24:23 +0200

    commit sauvegarde, seulement du premier coup mdr

--- Assets/Scripts/Serialisation/SaveWorld.cs
+++ Assets/Scripts/Serialisation/SaveWorld.cs
@@ -26,26 +26,26 @@ public class SaveWorld : MonoBehaviour
 	{
 		GameObject[] all = (GameObject[]) GameObject.FindObjectsOfType(typeof(GameObject));
 		SerializableObj[] objs = new SerializableObj[all.Length];
-		Debug.Log (all);
+
 	    for (int i = 0; i < all.Length; i++)
-			{
-	      objs[i] = new SerializableObj();
+		{
+	     	objs[i] = new SerializableObj();
 
-	      objs[i].x = all[i].transform.position.x;
-				objs[i].y = all[i].transform.position.y;
-				objs[i].z = all[i].transform.position.z;
+	      	objs[i].x = all[i].transform.position.x;
+			objs[i].y = all[i].transform.position.y;
+			objs[i].z = all[i].transform.position.z;
 
-				objs[i].xRotate = all[i].transform.rotation.eulerAngles.x;
-				objs[i].yRotate = all[i].transform.rotation.eulerAngles.y;
-				objs[i].zRotate = all[i].transform.rotation.eulerAngles.z;
+			objs[i].xRotate = all[i].transform.rotation.eulerAngles.x;
+			objs[i].yRotate = all[i].transform.rotation.eulerAngles.y;
+			objs[i].zRotate = all[i].transform.rotation.eulerAngles.z;
 
-				objs[i].objName = all[i].name;
+			objs[i].objName = all[i].name;
 
-	      objs[i].audio = all[i].GetComponent<AudioObject>();
-	      objs[i].link = all[i].GetComponent<LinkObject>();
-	      objs[i].image = all[i].GetComponent<ImageObject>();
-	      objs[i].text = all[i].GetComponent<TextObject>();
-	      objs[i].video = all[i].GetComponent<VideoObject>();
+	      	objs[i].audio = all[i].GetComponent<AudioObject>();
+	      	objs[i].link = all[i].GetComponent<LinkObject>();
+	      	objs[i].image = all[i].GetComponent<ImageObject>();
+	      	objs[i].text = all[i].GetComponent<TextObject>();
+	      	objs[i].video = all[i].GetComponent<VideoObject>();
 	    }
 
 		BinaryFormatter bf = new BinaryFormatter();
