1bbbf9fe2cf56eb646a8090455ac27b3e260b3bd
victor <victorpichon@free.fr>
2016-11-17 15:04:07 +0100

    j'avais oublié d'add les files kek :lenny:

---
+++ Assets/Scripts/Models/Others/OtherObjectHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Bolt;
+
+[System.Serializable]
+public class OtherObjectHandler : Bolt.EntityBehaviour<IOtherObjectState> {
+
+	public override void Attached()
+	{
+	}
+
+	public override void SimulateOwner()
+	{
+	}
+
+	public void test(NetworkId objNetworkId, BoltEntity entity)
+	{
+	}
+}
