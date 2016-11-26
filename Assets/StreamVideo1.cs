17aa556f33b9983c2d8b8b3cd7e021758e9670fa
William Nayrole <William Nayrole>
2016-11-26 01:21:30 +0100

    il manquait des fichiers

---
+++ Assets/StreamVideo1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreamVideo1 : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () 
+	{
+		var www = new WWW ("http://techslides.com/demos/sample-videos/small.ogv");
+		var movieTexture = www.movie;
+		while (!movieTexture.isReadyToPlay)
+			yield return movieTexture;
+
+		GetComponent<Renderer>().material.mainTexture = movieTexture;
+		movieTexture.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
