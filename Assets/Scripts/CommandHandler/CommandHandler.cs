709c4c51a675b7c7ca159541d68c8038cb598fec
Bakunine <wilfried.jaguenaud@epitech.eu>
2017-01-14 15:58:01 +0100

    Users and commands

--- Assets/Scripts/CommandHandler/CommandHandler.cs
+++ Assets/Scripts/CommandHandler/CommandHandler.cs
@@ -1,16 +1,13 @@
+using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-using UnityEngine;
-
-///<summary>
-///Handle all UNIX commands
-///</summary>
-public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TODO
+public class CommandHandler
 {
 	private Dictionary<string, Delegate> commands;
+	private UserInfo _user = new UserInfo();
 
 	public CommandHandler()
 	{
@@ -21,12 +18,14 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 		commands.Add("ls", new Func<List<string>, string>(ls));
 		commands.Add("mkdir", new Func<List<string>, string>(mkdir));
 		commands.Add("mv", new Func<List<string>, string>(mv));
-		// commands.Add("ps",  new Func<List<string>, string>(ps));
 		commands.Add("pwd", new Func<List<string>, string>(pwd));
 		commands.Add("rm", new Func<List<string>, string>(rm));
 		commands.Add("rmdir", new Func<List<string>, string>(rmdir));
 		commands.Add("touch", new Func<List<string>, string>(touch));
-		//commands.Add("whoami", new Func<List<string>, string>(whoami));
+		commands.Add("whoami", new Func<List<string>, string>(whoami));
+		commands.Add("changepseudo", new Func<List<string>, string>(changepseudo));
+		commands.Add("rights", new Func<List<string>, string>(rights));
+		commands.Add("listrights", new Func<List<string>, string>(listrights));
 	}
 
 	public string CallFunction(List<string> cmdLine)
@@ -48,20 +47,30 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 		return del.DynamicInvoke(cmdLine).ToString();
 	}
 
-	// TODO: Gérer les accès pour cette fonction
 	private string cat(List<string> args)
 	{
+		string result = "";
+
 		if (args.Count == 0)
-			return "cat command needs 1 argument.\n"; // TODO: plus verbose
+			return "cat command needs at least 1 argument.\n"; // TODO: plus verbose
 		else
 		{
-			FileUtils.File file = new FileUtils.File(args[0]);
-
-			if (!file.IsFile())
-				return "File not found\n"; // TODO: plus verbose
-
-			return file.GetContent();
+			for (int i = 0; i < args.Count; ++i)
+			{
+				string path = (args[i][0] == '/' ? PathUtils.GetPathFromAbsolute(PathUtils.RootPath + args[i]) : PathUtils.GetPathFromAbsolute(PathUtils.CurrPath + "/" + args[i]));
+				if (path.Contains(PathUtils.RootPath))
+				{
+					FileUtils.File file = new FileUtils.File(path);
+					if (!file.IsFile())
+						result += "File not found\n"; // TODO: plus verbose
+					else
+						result += file.GetContent() + "\n";
+				}
+				else
+					result += "Cannot access " + args[i] + " : Permission denied.\n";
+			}
 		}
+		return result;
 	}
 
 	private string cd(List<string> args)
@@ -91,25 +100,27 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 	// TODO: Gérer les accès pour cette fonction
 	private string cp(List<string> args) // TODO: à pousser ?
 	{
-		if (args.Count == 0)
-			return "cp command needs 1 (or 2) argument(s).\n"; // TODO: plus verbose
+		if (args.Count != 2)
+			return "cp command needs 2 argument.\n"; // TODO: plus verbose
 		else
 		{
-			FileUtils.File file1 = new FileUtils.File(args[0]);
+			string path = (args[0][0] == '/' ? PathUtils.GetPathFromAbsolute(PathUtils.RootPath + args[0]) : PathUtils.GetPathFromAbsolute(PathUtils.CurrPath + "/" + args[0]));
+			if (!path.Contains(PathUtils.RootPath))
+				return "Cannot access " + args[0] + " : Permission denied.\n";
+			FileUtils.File file1 = new FileUtils.File(path);
+
+			path = (args[1][0] == '/' ? PathUtils.GetPathFromAbsolute(PathUtils.RootPath + args[1]) : PathUtils.GetPathFromAbsolute(PathUtils.CurrPath + "/" + args[1]));
+			if (!path.Contains(PathUtils.RootPath))
+				return "Cannot access " + args[1] + " : Permission denied.\n";
+			FileUtils.File file2 = new FileUtils.File(path);
 
 			if (!file1.IsFile())
 				return file1.ProjectPath + " : No such file or directory.\n"; // TODO: plus verbose
-
-			FileUtils.File file2 = new FileUtils.File(file1.GetName());
-
-			if (args.Count >= 2)
-				file2 = new FileUtils.File(args[1]);
-
 			if (file2.IsFile())
 				return file2.ProjectPath + " : already exists.\n"; // TODO: plus verbose
 
 			File.Copy(file1.RealPath, file2.RealPath); // TODO: à mettre dans les utils
-			return file1.ProjectPath + "copied to " + file2.ProjectPath + ".\n"; // TODO: plus verbose
+			return file1.ProjectPath + " copied to " + file2.ProjectPath + ".\n"; // TODO: plus verbose
 		}
 	}
 
@@ -189,20 +200,23 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 	// TODO: Gérer les accès pour cette fonction
 	private string mv(List<string> args) // TODO: à faire
 	{
-		if (args.Count == 0)
-			return "mv command needs 1 (or 2) argument(s).\n"; // TODO: plus verbose
+		if (args.Count != 2)
+			return "mv command needs 2 arguments.\n"; // TODO: plus verbose
 		else
 		{
-			FileUtils.File file1 = new FileUtils.File(args[0]);
+			string path = (args[0][0] == '/' ? PathUtils.GetPathFromAbsolute(PathUtils.RootPath + args[0]) : PathUtils.GetPathFromAbsolute(PathUtils.CurrPath + "/" + args[0]));
+			if (!path.Contains(PathUtils.RootPath))
+				return "Cannot access " + args[0] + " : Permission denied.\n";
+			FileUtils.File file1 = new FileUtils.File(path);
+
+			path = (args[1][0] == '/' ? PathUtils.GetPathFromAbsolute(PathUtils.RootPath + args[1]) : PathUtils.GetPathFromAbsolute(PathUtils.CurrPath + "/" + args[1]));
+			if (!path.Contains(PathUtils.RootPath))
+				return "Cannot access " + args[1] + " : Permission denied.\n";
+			FileUtils.File file2 = new FileUtils.File(path);
+
 
 			if (!file1.IsFile())
 				return file1.ProjectPath + " : No such file or directory.\n"; // TODO: plus verbose
-
-			FileUtils.File file2 = new FileUtils.File(file1.GetName());
-
-			if (args.Count >= 2)
-				file2 = new FileUtils.File(args[1]);
-
 			if (file2.IsFile())
 				return file2.ProjectPath + " : already exists.\n"; // TODO: plus verbose
 
@@ -210,25 +224,6 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 			return file1.ProjectPath + " moved to " + file2.ProjectPath + "\n"; // TODO: plus verbose
 		}
 	}
-
-	/*private void ps()
-	{
-		Process[] processes = Process.GetProcesses();
-
-    Console.WriteLine("-----------------------------------------------------");
-    Console.WriteLine("{0, -8} {1, -30} {2, -10}", "PID", "Process Name", "Status");
-    Console.WriteLine("-----------------------------------------------------");
-
-    foreach (Process process in processes)
-      try
-      {
-        Console.WriteLine("{0, -8} {1, -30} {2, -10}", process.Id, process.ProcessName, process.Responding ? "Running" : "IDLE");
-      }
-      catch (Exception)
-      {
-        continue; // TODO: gérer les exception, stp
-      }
-	}*/
 
 	private string pwd(List<string> args)
 	{
@@ -322,8 +317,26 @@ public class CommandHandler // TODO: à update (@wilfried) + gérer tout les TOD
 		return result;
 	}
 
-	/*private string whoami(List<string> args)
+	private string whoami(List<string> args)
 	{
-		return Environment.UserName + "\n";
-	}*/
+		return _user.pseudo + "\n";
+	}
+
+	private string changepseudo(List<string> args)
+	{
+		if (args.Count == 0)
+			return "changepseudo command needs at least 1 arguments.\n"; // TODO: plus verbose		_user.pseudo = args[]
+	 	_user.pseudo = args[0];
+		return "Pseudo changed\n";
+	}
+
+	private string rights(List<string> args)
+	{
+		return "Your right is : " + _user.rights + "\n";
+	}
+
+	private string listrights(List<string> args)
+	{
+		return "The rights are the following :\n" + _user.r.listRights();
+	}
 }
